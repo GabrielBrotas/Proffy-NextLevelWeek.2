e4c17a3b6eae60a733af9702a2228dfdce43a102
GabrielBrotas <gabrielbrotas22@gmail.com>
2020-11-13 21:28:35 -0300

    Show teacher List

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,36 +1,59 @@
 import React from 'react'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api'
 
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    name: string;
+    avatar: string;
+    whatsapp: string;
+    cost: number;
+    bio: string;
+    subject: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+
+    function createNewConnection() {
+        api.post('/connections', {user_id: teacher.id})
+    }
+  
     return (
         <article className="teacher-item">
                     <header>
-                        <img src="https://avatars1.githubusercontent.com/u/63565773?s=460&u=47a9f80c4fc321d44adcc314993d9ebfe8a64497&v=4" alt="Gabriel" />
+                        <img src={teacher.avatar} alt={teacher.name} />
 
                         <div>
-                            <strong>Gabriel Brotas</strong>
-                            <span>Empreendorismo</span>
+                            <strong>{teacher.name}</strong>
+                            <span>{teacher.subject}</span>
                         </div>
                     </header>
 
                     <p>
-                        Especialista em educação financeira e gerenciamento de Negócios.
-                        <br />
-                        Inscreva - se para aprender tudo sobre gestão de negócios e como faturar muito com franquias.
+                        {teacher.bio}
                     </p>
 
                     <footer>
                         <p>
                             Preço/hora
-                            <strong>R$ 100,00</strong>
+                            <strong>R$ {teacher.cost}</strong>
                         </p>
-                        <button type="button">
+
+                        <a 
+                            href={`https://api.whatsapp.com/send?phone=${teacher.whatsapp}`} 
+                            onClick={createNewConnection}
+                            target="_blank"
+                        >
                             <img src={whatsappIcon} alt="Contato Wpp" />
                             Entrar em Contato
-                        </button>
+                        </a>
                     </footer>
                 </article>
     )
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,24 +1,49 @@
-import React from 'react'
-import Input from '../../components/Input'
+import React, { FormEvent, useState } from 'react'
 
+import api from '../../services/api'
 // Components
 import PageHeader from '../../components/PageHeader'
 import Select from '../../components/Select'
-import TeacherItem from '../../components/TeacherItem'
+import TeacherItem, { Teacher } from '../../components/TeacherItem'
+import Input from '../../components/Input'
 
 import './styles.css'
 
 function TeacherList() {
+
+    const [subject, setSubject] = useState('')
+    const [week_day, setWeekDay] = useState('')
+    const [time, setTime] = useState('')
+    const [teachers, setTeachers] = useState([])
+
+    async function searchTeachers(e: FormEvent) {
+        e.preventDefault();
+        
+        const response = await api.get('classes', {
+            // passar querys via parametros
+            params: {
+                subject, 
+                week_day,
+                time
+            }
+        })
+
+        setTeachers(response.data)
+        
+    }
+    
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponívels">
 
                 {/* Child do componente PageHeader */}
-                <form id="search-teachers">
+                <form id="search-teachers" onSubmit={searchTeachers}>
 
                     <Select
                         label="Matéira" 
                         name="subject" 
+                        value={subject}
+                        onChange={ e => setSubject(e.target.value)}
                         options={[
                             { value: 'Artes', label: "Artes"},
                             { value: 'Matematica', label: "Matematica"},
@@ -31,6 +56,8 @@ function TeacherList() {
                     <Select
                         label="Dia da Semana" 
                         name="week_day" 
+                        value={week_day}
+                        onChange={ e => setWeekDay(e.target.value)}
                         options={[
                             { value: '0', label: "Domingo"},
                             { value: '1', label: "Segunda-Feira"},
@@ -46,17 +73,22 @@ function TeacherList() {
                     type="time"
                     label="Horário" 
                     name="time" 
+                    value={time}
+                    onChange={e => setTime(e.target.value)}
                     />
                 
+                    <button type="submit">Buscar</button>
+                        
                 </form>
 
             </PageHeader>
 
             <main>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+
+                {teachers.map( (teacher: Teacher) => (
+                    <TeacherItem key={teacher.id} teacher={teacher} />
+                ))}
+
             </main>
         </div>
     )
